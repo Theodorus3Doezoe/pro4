426a3a6c559c439e92e6d6c808d6b679d68ef6bd
Alex <alexanderdelaat8@gmail.com>
2025-05-08 15:21:12 +0200

    Header Component gemaakt

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Header.css'
+import logo from "../../assets/logo_trans.png"
+import profile from "../../assets/profile.png"
+import { Link } from 'react-router-dom'
+
 
 export default function Header() {
   return (
-    <div>
-      
+    <div className="header_container">
+      <Link to={'/'}>
+        <img class="header_img" src={logo}/>
+      </Link>
+      <Link to={'/profile'}>
+        <img class="header_img" id="profile_img" src={profile}/>
+      </Link>
     </div>
   )
 }
--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+332a64a49c74f34e6f82fb8d5c03bf68bf3e7343")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a99da203af7ff4c4092cc321f03bb9747fdd7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
