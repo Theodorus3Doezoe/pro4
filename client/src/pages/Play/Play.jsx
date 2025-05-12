7780dd55e082fd1cc9f7dd48a642e9ce66a02b69
ThijnvanWesterhuis <thijn.westerhuis@gmail.com>
2025-05-12 13:11:49 +0200

    play page progress

--- client/src/pages/Play/Play.jsx
+++ client/src/pages/Play/Play.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './Play.css'
 import Youtube from 'react-youtube'
+import Header from '../../components/Header/Header'
+import heartIcon from '../../assets/heart-rate.png'
 
 export default function Play() {
   const options = {
-    height: '400',
-    width: '650',
+    height: '100%',
+    width: '100%',
     playerVars: {
       autoplay: 1,
       controls: 1,
+      rel: 0,
+      modestbranding: 1,
     }
   }
 
@@ -17,8 +21,22 @@ export default function Play() {
   };
   
   return (
-    <div className='play-container'>
-      <Youtube videoId='F1JcJp4u5nc' opt={options} onReady={handleReady} id="video"/>
+    <div className='play-page'>
+      <Header/>
+      <h1 className='score-title'>SCORE:</h1>
+      
+      <div className='main-content'>
+        <div className='video-container'>
+        <Youtube videoId='F1JcJp4u5nc' opts={options} onReady={handleReady} id="video"/>
+        </div>
+        <div className='side-container'>
+          <div className='webcam-container'/>
+          <div className='heartrate-container'>
+            <img className='heart-icon' src={heartIcon} alt='Heart Icon' />
+            <span className='heartrate-number'>175</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
