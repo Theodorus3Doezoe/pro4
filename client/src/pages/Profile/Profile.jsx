9b9ad43a17ae895ad2630bc2cf60f40caa3a2fa1
Alex <alexanderdelaat8@gmail.com>
2025-05-15 15:14:33 +0200

    update profiel pagina

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Profile.css'
 import Header from '../../components/Header/Header'
+import placeholder from '../../assets/placeholder.jpg'
 
 export default function Profile() {
   return (
     <>
       <Header/>
       <div className='home_container'>
-        <div className='video_container'>
-          <div className="image_container"></div>
-        </div>
-        <div className="profile_picture">
+        <div className="profile_container">
+          <img src={placeholder} alt="" id="profile_image" />
+          <h2 className="profile_text" id="name">Alex</h2>
+          <h2 className="profile_text" id="score_text">Score: <span id="score">99999</span></h2>
         </div>
       </div>
     </>
