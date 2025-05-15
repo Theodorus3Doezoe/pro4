c629ae4db58125d05d5c319b85492c0c692faf2d
Theodorus3Doezoe <91803214+Theodorus3Doezoe@users.noreply.github.com>
2025-05-15 17:52:38 +0200

    Delete server directory
    Krijg teveel conflicts dus even proberen de backend opnieuw te pushen

--- server/data/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using AccountApi.Models;
-
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    public DbSet<User> Users { get; set; }
-}
