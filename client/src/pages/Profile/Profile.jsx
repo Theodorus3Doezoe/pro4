661b56d6976a4c3586b68e238b153e8e814457ab
Alex <alexanderdelaat8@gmail.com>
2025-05-15 13:37:44 +0200

    Home af, begin profiel

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Profile.css'
+import Header from '../../components/Header/Header'
 
 export default function Profile() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header/>
+      <div className='home_container'>
+        <div className='video_container'>
+          <div className="image_container"></div>
+        </div>
+        <div className="profile_picture">
+        </div>
+      </div>
+    </>
   )
 }
+
